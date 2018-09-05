fc9c314fdeb06f8f1ea2725be5e10919a23421ec
Tuan CONG LE <tlcong@linagora.com>
2018-09-05 14:48:00 +0700

    linagora/lgs/openpaas/linagora.esn.admin#462 Store mail repository path prefixes as the contants

---
+++ frontend/app/dlp/inbox-james-dlp.constants.js
@@ -0,0 +1,9 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.unifiedinbox.james')
+
+  .constant('INBOX_JAMES_DLP_MAIL_REPOSITORY_PATH_PREFIXES', {
+    QUARANTINE: 'var/mail/dlp/quarantine'
+  });
+})(angular);

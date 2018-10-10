98939000e4a073cd963dd32a2f08e02172d2e484
Christophe Hamerling <chamerling@linagora.com>
2018-10-10 11:42:33 +0200

    linagora/lgs/openpaas/linagora.esn.unifiedinbox#947 Fix the module name

--- frontend/app/dlp/rejected/inbox-james-dlp-rejected.component.js
+++ frontend/app/dlp/rejected/inbox-james-dlp-rejected.component.js
@@ -1,7 +1,7 @@
 (function(angular) {
   'use strict';
 
-  angular.module('linagora.esn.unifiedinbox')
+  angular.module('linagora.esn.unifiedinbox.james')
     .component('inboxJamesDlpRejected', {
       templateUrl: '/linagora.esn.unifiedinbox.james/app/dlp/rejected/inbox-james-dlp-rejected.html',
       controller: 'inboxJamesDlpRejectedController'
